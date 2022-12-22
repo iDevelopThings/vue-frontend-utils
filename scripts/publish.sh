ca7314b0e0298b1ce78ac3bdfbc4804013a7f706
Sam Parton <sam@idevelopthings.com>
2022-12-22 10:52:56 +0000

    Update publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,13 +6,13 @@ echo "Running build script..."
 
 ./scripts/build.sh
 
-if [ -z "$(git status --porcelain)" ]; then
-else
+#if [ -z "$(git status --porcelain)" ]; then
+#else
   npm version patch -m "%s"
   git add .
   git commit -m "Publishing new version"
   git push
-fi
+#fi
 
 echo "Publishing..."
 npm publish
