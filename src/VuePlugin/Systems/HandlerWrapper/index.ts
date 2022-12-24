a3eaf73a9edcedf6fbda91a41e2ebc0991723df7
Sam Parton <sam@idevelopthings.com>
2022-12-24 12:45:56 +0000

    Forgot to define exports

--- src/VuePlugin/Systems/HandlerWrapper/index.ts
+++ src/VuePlugin/Systems/HandlerWrapper/index.ts
@@ -1,5 +1,6 @@
-export {useAsyncHandler, type IAsyncHandlerHook, type IMemoizedAsyncHandlerHook, useMemoizedAsyncHandler} from "./Handlers";
 export {PromiseCancelledError, PromiseTimeoutError, type IPromisified, promisify} from "./Promisify";
 export {type BaseState, BaseHandler, type IBaseHandler} from "./BaseHandler";
 export {type IAsyncHandler, type AsyncState, AsyncHandler} from "./AsyncHandler";
 export {MemoizedAsyncHandler, type MemoizedAsyncHandlerOptions, type MemoizeCache, type IMemoizedAsyncHandler, type MemoizeAsyncState, type KeyGeneratorType} from "./MemoizedAsyncHandler";
+export {useAsyncHandler, type IAsyncHandlerHook} from "./Handlers";
+export {useMemoizedAsyncHandler, type IMemoizedAsyncHandlerHook} from "./Handlers";
