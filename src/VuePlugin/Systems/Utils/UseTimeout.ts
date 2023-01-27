71663cf7c420a5a96c7f96a545cd12efce773f4e
Sam Parton <sam@idevelopthings.com>
2023-01-27 16:51:28 +0000

    - Implement useInterval

--- src/VuePlugin/Systems/Utils/UseTimeout.ts
+++ src/VuePlugin/Systems/Utils/UseTimeout.ts
@@ -40,6 +40,44 @@ export function useTimeout(fn: Function, ms: number) {
 	};
 }
 
+export function useInterval(fn: Function, ms: number) {
+	const interval = ref<number>(null);
+
+	function start() {
+		if (interval.value) {
+			return;
+		}
+		interval.value = setInterval(fn, ms);
+	}
+
+	function setTime(newMs: number) {
+		ms = newMs;
+		if (interval.value) {
+			stop();
+			start();
+		}
+	}
+
+	function stop() {
+		if (!interval.value) {
+			return;
+		}
+		clearInterval(interval.value);
+		interval.value = null;
+	}
+
+	const comp = getCurrentInstance();
+	if (comp) {
+		onBeforeUnmount(stop, comp);
+	}
+
+	return {
+		start,
+		stop,
+		setTime,
+	};
+}
+
 export function useDebounce(fn: Function, ms: number) {
 	const timeout = ref<number>(null);
 
